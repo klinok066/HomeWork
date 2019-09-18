a5c2c53e51f844e0d484f678e62baf812dee6154
klinok066 <50661828+klinok066@users.noreply.github.com>
2019-09-18 23:29:09 +0500

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -6,13 +6,13 @@ public class Main1 {
     }
 
     static void makeArray(){
-        double [] array = new double[12];
+
+        int [] array = new int[12];
         for(int i = 0; i < 12; i++) {
-                array[i] = (Math.random() * ((15 + 15 ))- 15);
+                array[i] = (int)(Math.random() * ((15 + 15 ))- 15);
             }
-            
         int number = 0;
-        double max = -16;
+        int max = -16;
         for(int i = 0; i < 12; i++) {
             if(array[i] > max){
                 max = array[i];
